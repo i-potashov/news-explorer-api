3213bbafb1583d22dfc4711329ce49494da17cd8
i-potashov <i.ptshv@gmail.com>
2020-05-29 06:01:37 +0300

    Fix jwt

--- configuration/config.js
+++ configuration/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT === 'production' ? process.env.PORT : 3000;
 const DB = process.env.NODE_ENV === 'production' ? process.env.DATA_BASE : 'mongodb://localhost:27017/newsdb';
-const JWT_KEY = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'secret_key';
+const JWT_KEY = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'a504cd18bc794250f0e69430f5b8f57ce668ec67e1e10e51341cc6885126553e';
 
 module.exports = {
   DB,
