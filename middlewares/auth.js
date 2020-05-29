e93def65e761cffc84bd47b76dc7bb783e7959b2
i-potashov <i.ptshv@gmail.com>
2020-05-29 05:16:16 +0300

    Fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,17 @@ const { JWT_KEY } = require('../configuration/config');
 const LoginError = require('../errors/LoginError');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const { Authorization } = req.headers;
+  if (!Authorization || !Authorization.startsWith('Bearer ')) {
     throw new LoginError(USER_NOT_FOUND);
   }
-  const token = authorization.replace('Bearer ', '');
+  const token = Authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_KEY);
   } catch (err) {
     throw new LoginError(USER_NOT_FOUND);
   }
-
   req.user = payload;
 
   return next();
